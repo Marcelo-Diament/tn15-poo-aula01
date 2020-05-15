5616eb4257349bf40c3e5ff3edadc5498e223d33
Marcelo Diament <marcelodiament@gmail.com>
2020-05-15 20:19:41 -0300

    Instanciando um objeto e declarando suas propriedades a partir da classe PessoaX

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 
-    
+    // PROPRIEDADES
+    echo "<h2>PROPRIEDADES</h2>";
+    // Chamando/trazendo o arquivo da classe para que possamos instanciar um objeto a partir dela.
+    require_once("./classes/PessoaX.php");
+
+    // Vamos instanciar uma pessoaX (objeto) a partir da classe PessoaX:
+    $fulano = new PessoaX();
+    $fulano->nome = "Fulano";
+    $fulano->sobrenome = "da Silva";
+    echo $fulano->nome . " " . $fulano->sobrenome;
+    echo "<br/><hr/><br/>";
+
+    // Vamos instanciar uma nova pessoaX (objeto) a partir da classe PessoaX:
+    $beltrano = new PessoaX();
+    $beltrano->nome = "Beltrano";
+    $beltrano->sobrenome = "da Silva";
+    echo $beltrano->nome . " " . $beltrano->sobrenome;
+    echo "<br/><hr/><br/>";
 
 ?>
